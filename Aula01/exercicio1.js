b36e200b0645b6389697647f07735cf0430c509c
valentina <vaahxx@gmail.com>
2019-09-17 21:39:06 -0300

    exercicio 1

---
+++ Aula01/exercicio1.js
@@ -0,0 +1,9 @@
+// Número maior que 10
+
+const numero = prompt('Digite um numero');
+
+if (numero >= 10) {
+    alert(`${numero} é maior ou igual a dez`);
+} else {
+    alert(`${numero} é menor do que dez`);
+}
