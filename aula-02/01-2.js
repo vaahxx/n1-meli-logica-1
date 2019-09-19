d2d801d1171b06713623b3efb35fa63d04e58e30
valentina <vaahxx@gmail.com>
2019-09-18 22:01:46 -0300

    refatorando o exercicio 01-2

--- aula-02/01-2.js
+++ aula-02/01-2.js
@@ -11,24 +11,24 @@ if (numero == palpite1) {
         alert(`o número sorteado é MAIOR do que ${palpite1}`)
     } else {
         alert(`o número sorteado é MENOR do que ${palpite1}`)
+        //SEGUNDA  TENTATIVA
+        const palpite2 = parseInt(prompt('Digite um número de 1 a 10'));
+        if (numero == palpite2) {
+            alert('voce acertou!');
+        } else {
+            alert('tente de novo');
+            if (numero > palpite2) {
+                alert(`o número sorteado é MAIOR do que ${palpite2}`)
+            } else {
+                alert(`o número sorteado é MENOR do que ${palpite2}`)
+                //TERCEIRA TENTATIVA
+                const palpite3 = parseInt(prompt('Digite um número de 1 a 10'));
+                if (numero == palpite3) {
+                    alert('voce acertou');
+                } else {
+                    alert('suas chances acabaram. o número aleatório era ' + numero);
+                }
+            }
+        }
     }
-}
-
-const palpite2 = parseInt(prompt('Digite um número de 1 a 10'));
-if (numero == palpite2) {
-    alert('voce acertou!');
-} else {
-    alert('tente de novo');
-    if (numero > palpite2) {
-        alert(`o número sorteado é MAIOR do que ${palpite2}`)
-    } else {
-        alert(`o número sorteado é MENOR do que ${palpite2}`)
-    }
-}
-
-const palpite3 = parseInt(prompt('Digite um número de 1 a 10'));
-if (numero == palpite3) {
-    alert('voce acertou');
-} else {
-    alert('suas chances acabaram. o número aleatório era ' + numero);
 }
