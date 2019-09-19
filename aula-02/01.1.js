d965b593fda1e63f665700d387a8bdb2bcaf7e11
valentina <vaahxx@gmail.com>
2019-09-18 21:36:33 -0300

    exercicio 01.1 terminado

--- aula-02/01.1.js
+++ aula-02/01.1.js
@@ -1,12 +1,16 @@
 // 1.1. Escreva um programa que sorteia um número de 1 a 10, dá duas chances para que o usuário acerte, mas informa se o número sorteado é maior ou menor que o primeiro palpite.
 
+const numero = Math.floor(Math.random() * 11);
+
 const palpite1 = parseInt(prompt('Digite um número de 1 a 10'));
 if (numero == palpite1) {
     alert('voce acertou!');
 } else {
     alert('tente de novo');
-    if () {
-        console.log(`o número sorteado é maior do que ${palpite1}`)
+    if (numero > palpite1) {
+        alert(`o número sorteado é MAIOR do que ${palpite1}`)
+    } else {
+        alert(`o número sorteado é MENOR do que ${palpite1}`)
     }
 }
 
