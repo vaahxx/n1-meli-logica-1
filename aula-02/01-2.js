ba057341ba9726f006cc1372d52ccb8f265be147
valentina <vaahxx@gmail.com>
2019-09-18 21:50:40 -0300

    criando o exercicio 1.2

---
+++ aula-02/01-2.js
@@ -0,0 +1,22 @@
+// 1.2. Faça o mesmo programa anterior, agora com 3 palpites
+
+const numero = Math.floor(Math.random() * 11);
+
+const palpite1 = parseInt(prompt('Digite um número de 1 a 10'));
+if (numero == palpite1) {
+    alert('voce acertou!');
+} else {
+    alert('tente de novo');
+    if (numero > palpite1) {
+        alert(`o número sorteado é MAIOR do que ${palpite1}`)
+    } else {
+        alert(`o número sorteado é MENOR do que ${palpite1}`)
+    }
+}
+
+const palpite2 = parseInt(prompt('Digite um número de 1 a 10'));
+if (numero == palpite2) {
+    alert('voce acertou');
+} else {
+    alert('suas chances acabaram. o número aleatório era ' + numero);
+}
