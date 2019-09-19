e25cc525c07cd94b547b1a99b09c57fb2adbe02c
valentina <vaahxx@gmail.com>
2019-09-18 21:58:05 -0300

    refatoração do exercicio 01-1

--- aula-02/01-1.js
+++ aula-02/01-1.js
@@ -11,12 +11,12 @@ if (numero == palpite1) {
         alert(`o número sorteado é MAIOR do que ${palpite1}`)
     } else {
         alert(`o número sorteado é MENOR do que ${palpite1}`)
+        //SEGUNDA TENTATIVA
+        const palpite2 = parseInt(prompt('Digite um número de 1 a 10'));
+        if (numero == palpite2) {
+            alert('voce acertou');
+        } else {
+            alert('suas chances acabaram. o número aleatório era ' + numero);
+        }
     }
-}
-
-const palpite2 = parseInt(prompt('Digite um número de 1 a 10'));
-if (numero == palpite2) {
-    alert('voce acertou');
-} else {
-    alert('suas chances acabaram. o número aleatório era ' + numero);
 }
